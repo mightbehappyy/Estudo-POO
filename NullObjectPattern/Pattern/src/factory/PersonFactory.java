723bbfe1ba70874343c798d0ab5a9b43df9de091
Pedro <pedroluiz0201690@gmail.com>
2023-11-08 18:53:36 -0300

    m

--- NullObjectPattern/Pattern/src/factory/PersonFactory.java
+++ NullObjectPattern/Pattern/src/factory/PersonFactory.java
@@ -4,9 +4,10 @@ import person.Person;
 
 public class PersonFactory {
     private Person person;
+
     public void createPerson(Person person) {
-        if(person != null) {
-            person = new Person(null, null)
+        if (person != null) {
+            person = new Person(null, null);
         }
     }
 }
--- Q5/src/App.java
+++ Q5/src/App.java
@@ -1,9 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         VetorOrdenado v1 = new VetorOrdenado(5);
-        v1.insert("pEDRO");
-        v1.insert("Luiz");
-        v1.insert("Rodrigues");
         System.out.println(v1.get(0));
     }
 }
